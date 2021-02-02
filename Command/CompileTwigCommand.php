37098f4959d14a8b93119cb7b0c1eb7fbcc46d32
Andrejs Kovtuns <andkovt@gmail.com>
2021-02-02 19:48:41 +0200

    Updated compile command to support using stdin as a source

--- Command/CompileTwigCommand.php
+++ Command/CompileTwigCommand.php
@@ -4,8 +4,10 @@
 namespace JMS\TwigJsBundle\Command;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Exception\InvalidOptionException;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use TwigJs\CompileRequest;
 use TwigJs\CompileRequestHandler;
@@ -32,16 +34,34 @@ class CompileTwigCommand extends Command
     {
         $this
             ->setDefinition([
-                new InputArgument('name', InputArgument::REQUIRED | InputArgument::IS_ARRAY, 'The template name')
+                new InputArgument('source', InputArgument::OPTIONAL | InputArgument::IS_ARRAY, 'Template source'),
+                new InputOption('name', null, InputOption::VALUE_OPTIONAL, 'Template name')
             ])
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        foreach ($input->getArgument('name') as $name) {
-            $compileRequest = new CompileRequest($name, null);
-            $output->write($this->compileRequestHandler->process($compileRequest));
+        if (count($input->getArgument('source')) > 0) {
+            foreach ($input->getArgument('source') as $name) {
+                $compileRequest = new CompileRequest($name, null);
+                $output->write($this->compileRequestHandler->process($compileRequest));
+            }
+
+            return;
         }
+
+        if (!$input->hasOption('name')) {
+            throw new InvalidOptionException("Option 'name' when using stdin as source");
+        }
+
+        $source = $this->readFromStdin();
+        $compileRequest = new CompileRequest($input->getOption('name'), $source);
+        $output->write($this->compileRequestHandler->process($compileRequest));
+    }
+
+    private function readFromStdin(): string
+    {
+        return stream_get_contents(STDIN);
     }
 }
